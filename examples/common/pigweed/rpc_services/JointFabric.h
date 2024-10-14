2cb74f77157f17748bc227b2cd6419803f20e32e
Marian Chereji <marian.chereji@nxp.com>
2024-10-14 14:29:18 +0300

    examples/common/pigweed: Align joint_fabric RPC service interfaces to the RPC services defined by the protofile
    Signed-off-by: Marian Chereji <marian.chereji@nxp.com>

--- examples/common/pigweed/rpc_services/JointFabric.h
+++ examples/common/pigweed/rpc_services/JointFabric.h
@@ -10,14 +10,16 @@ class TestImpl final: public pw_rpc::nanopb::Test::Service<TestImpl> {
 
 class JointFabricServiceImpl : public pw_rpc::nanopb::JointFabric::Service<JointFabricServiceImpl> {
     public:
-        pw::Status OpenCommissioningWindow(const joint_fabric_TextMessage& request, joint_fabric_ErrorCode& response);
+        pw::Status OpenCommissioningWindow(const ::joint_fabric_OpenCommissioningWindowIn& request,
+            joint_fabric_ErrorCode& response);
 };
 
 class JointFabricControlServiceImpl : public pw_rpc::nanopb::JointFabricControl::Service<JointFabricControlServiceImpl> {
     public:
-        pw::Status OpenCommissioningWindow(const joint_fabric_TextMessage& request, joint_fabric_ErrorCode& response);
-        pw::Status CommissionDevice(const joint_fabric_TextMessage& request, joint_fabric_ErrorCode& response);
+        pw::Status OpenCommissioningWindow(const joint_fabric_OpenCommissioningWindowIn& request,
+            joint_fabric_OpenCommissioningWindowOut& response);
+        pw::Status CommissionDevice(const joint_fabric_CommissionDeviceIn& request,
+            joint_fabric_ErrorCode& response);
 };
 
 }
-
