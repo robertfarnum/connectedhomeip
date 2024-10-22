f828370d06aa28262b5723420262ad259e2e69fd
Marian Chereji <marian.chereji@nxp.com>
2024-10-22 14:53:56 +0300

    examples/jf-admin-app: Add ControlPlane abstraction class
    Signed-off-by: Marian Chereji <marian.chereji@nxp.com>

---
+++ examples/jf-admin-app/linux/ControlPlane.cpp
@@ -0,0 +1,15 @@
+
+#include "ControlPlane.h"
+#include "RpcClient.h"
+
+class JointFabricControl jf_control;
+
+class JointFabricControl & JointFabricControl::GetInstance(void)
+{
+    return jf_control;
+}
+
+CHIP_ERROR JointFabricControl::UpdateOperationalIdentity(void)
+{
+    return RpcUpdateOperationalIdentity();
+}
