86a915756c1ea4590cccfdbc0900ffa8403b4b7d
Marian Chereji <marian.chereji@nxp.com>
2024-11-11 16:39:51 +0200

    examples/jf-admin-app: Upgrade OpenCommissioningWindow RPC API to support BCM
    Signed-off-by: Marian Chereji <marian.chereji@nxp.com>

--- examples/jf-admin-app/linux/RpcServer.cpp
+++ examples/jf-admin-app/linux/RpcServer.cpp
@@ -1,23 +1,52 @@
 
 #include "pigweed/rpc_services/JointFabric.h"
 #include <lib/support/logging/CHIPLogging.h>
+#include <app/server/Server.h>
+#include <app/server/CommissioningWindowManager.h>
+
+using namespace chip;
 
 namespace joint_fabric {
 
 pw::Status JointFabricServiceImpl::OpenCommissioningWindow(const joint_fabric_OpenCommissioningWindowIn& request,
     joint_fabric_OpenCommissioningWindowOut& response)
 {
+    pw::Status pw_err;
+    CHIP_ERROR err(CHIP_NO_ERROR);
+
+    /* Reset response */
     response.manual_code[0] = 0;
 
-    ChipLogProgress(NotSpecified, "OpenCommissioningWindow() call from control plane");
-    ChipLogProgress(NotSpecified, "    - mode = %u", request.mode);
-    ChipLogProgress(NotSpecified, "    - window_timeout = %u", request.window_timeout);
-    if (request.mode == 1) {
-        ChipLogProgress(NotSpecified, "    - iterations = %u", request.iterations);
-        ChipLogProgress(NotSpecified, "    - discriminator = %u", request.discriminator);
+    /*
+     * TODO: Maybe check that parameters values (window timeout, discriminator,
+     * iterations) are in the valid range.
+     */
+    DeviceLayer::PlatformMgr().LockChipStack();
+    switch (request.mode) {
+    case 0: /* BCM */
+        err = Server::GetInstance().GetCommissioningWindowManager().OpenBasicCommissioningWindow(
+            System::Clock::Seconds32(request.window_timeout));
+        break;
+    case 1: /* ECM */
+        DeviceLayer::PlatformMgr().UnlockChipStack();
+        return pw::Status::Unimplemented();
+    default: /* Not a valid commissioning mode */
+        DeviceLayer::PlatformMgr().UnlockChipStack();
+        return pw::Status::InvalidArgument();
+    }
+    DeviceLayer::PlatformMgr().UnlockChipStack();
+
+    /* Check the OCW command response and return the proper error code */
+    switch (err.AsInteger()) {
+    case CHIP_NO_ERROR.AsInteger(): /* Successful */
+        pw_err = pw::OkStatus();
+        break;
+    default: /* Anything else just return "unknown error" */
+        pw_err = pw::Status::Unknown();
+        break;
     }
 
-    return pw::OkStatus();
+    return pw_err;
 }
 
 }
